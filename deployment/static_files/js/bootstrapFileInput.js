9bb03c7bc032639c448b3b83ec148784aa922dae
zyang <zyang@ablesky.com>
2013-05-22 16:28:07 +0800

    20130522_1627 second time change of file input

--- deployment/static_files/js/bootstrapFileInput.js
+++ deployment/static_files/js/bootstrapFileInput.js
@@ -25,7 +25,6 @@ $(function(){
 			var pathTxtWidth = parseInt(opts.width) - parseInt(opts.btnWidth) - 30 + 'px';
 			var $pathTxt = $('<input type="text" />');
 			var $browseBtn = $('<button type="button">浏&nbsp;&nbsp;览</button>');
-			var $fileInput = $('<input type="file"/>');
 			$pathTxt.css({
 					width: pathTxtWidth
 			}).attr({
@@ -35,17 +34,6 @@ $(function(){
 				'width': opts.btnWidth,
 				'margin-bottom': '10px'
 			});
-			$fileInput.css({
-				'width': opts.btnWidth,
-				'position': 'absolute',
-				'opacity': 0
-			});
-			if(opts.fileInputId){
-				$fileInput.attr({
-					id: opts.fileInputId,
-					name: opts.fileInputName
-				});
-			}
 			// safari内核浏览器无法触发file类型的input的click事件，所以只能用透明度为0的方式叠在按钮上
 			if($.browser.safari){
 				var $pathWrap = $('<div>').css({
@@ -59,30 +47,55 @@ $(function(){
 					overflow: 'hidden',
 					'margin-left': '23px'
 				});
-				$fileInput.css({
-					right: '5px'
-				});
 				$pathWrap.append($pathTxt);
-				$btnWrap.append($browseBtn).append($fileInput);
+				$btnWrap.append($browseBtn);
+				buildFileInput($btnWrap, opts);
 				$this.append($pathWrap).append($btnWrap);
 			} else {
 				$browseBtn.css({
 					'margin-left': '10px'
 				});
-				$fileInput.css({
-					left: '0px',
-					'z-index': -1
-				});
-				$this.append($pathTxt).append($browseBtn).append($fileInput);
+				$this.append($pathTxt).append($browseBtn);
+				buildFileInput($this, opts);
 				$browseBtn.click(function(){
-					$fileInput.click();
+					buildFileInput($this, opts);
+					$(this).siblings('input[type=file]').click();
 				});
 			}
 			
-			$fileInput.change(function(){
-				$this.attr('data-path', this.value);
-				$pathTxt.val(this.value);
-			});
+			
+			/**
+			 * input type=file的onchange事件第二次就不管用了
+			 * chrome下还是会有问题，但是不影响功能，暂时不管了
+			 * 坑确实很深
+			 */
+			function buildFileInput($wrapper, opts) {
+				$wrapper.children('[type=file]').remove();
+				var $fileInput = $('<input type="file"/>');
+				$fileInput.css({
+					'width': opts.btnWidth,
+					'position': 'absolute',
+					'opacity': 0
+				}).attr({
+					id: opts.fileInputId,
+					name: opts.fileInputName
+				});
+				if($.browser.safari) {
+					$fileInput.css({
+						right: '5px'
+					});
+				} else {
+					$fileInput.css({
+						left: '0px',
+						'z-index': -1
+					});
+				}
+				$wrapper.append($fileInput);
+				$fileInput.change(function(){
+					$this.attr('data-path', this.value);
+					$pathTxt.val(this.value);
+				});
+			}
 		}
 	});
 	$.fn.bootstrapFileInput.defaults = {
@@ -91,4 +104,5 @@ $(function(){
 		fileInputId: '',
 		fileInputName: ''
 	}
+	
 })(jQuery);
