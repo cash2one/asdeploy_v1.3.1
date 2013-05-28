2ab13af1512d4de41860bde64df69d2c26f473ac
zyang <zyang@ablesky.com>
2013-05-28 14:06:06 +0800

    20130528_1405 login page

--- deployment/forms.py
+++ deployment/forms.py
@@ -20,14 +20,27 @@ class RegistrationForm(forms.Form):
     def clean_username(self):
         if 'username' in self.cleaned_data:
             username = self.cleaned_data.get('username')
-            if not re.search(r'^[a-zA-Z0-9]{3,30}$', username):
-                raise forms.ValidationError('用户名只能使用字符，数字和下划线')
+            if not re.search(r'^[a-zA-Z0-9_]{3,30}$', username):
+                raise forms.ValidationError('用户名只能使用字符，数字和下划线！')
             try:
                 User.objects.get(username = username)
             except:
                 return username
             raise forms.ValidationError('用户已存在!')
-        raise forms.ValidationError('用户名的长度必须在3到30之间')
+        raise forms.ValidationError('用户名的长度必须在3到30之间！')
+    
+    def clean_password1(self):
+        if 'password1' in self.cleaned_data:
+            password1 = self.cleaned_data.get('password1')
+            pwd_len = len(password1)
+            if pwd_len < 4 or pwd_len >= 30:
+                raise forms.ValidationError('密码长度需大于3并小于30！')
+            if not re.search(r'^[a-zA-Z0-9_]{4,30}$', password1):
+                raise forms.ValidationError('密码只能使用字符和数字！')
+            else: 
+                return password1
+        raise forms.ValidationError('密码不能为空！')
+        
 
     def clean_password2(self):
         if 'password1' in self.cleaned_data:
@@ -35,4 +48,4 @@ class RegistrationForm(forms.Form):
             password2 = self.cleaned_data.get('password2')
             if password1 == password2:
                 return password2
-        raise forms.ValidationError('两次输入的密码不相同!')
+        raise forms.ValidationError('两次输入的密码不相同！')
